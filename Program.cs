79ae6d385fec8e3bfd1d852208256c2d7af709af
362571390 <362571390@2260I-L29-001>
2026-02-17 12:57:38 -0500

    Done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Security;
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+using System.Security;
 
 namespace Topic_6
 {
@@ -12,7 +14,8 @@ namespace Topic_6
                 Console.WriteLine("Menu");
                 Console.WriteLine("1 for min and max questions");
                 Console.WriteLine("2 for bank");
-                Console.WriteLine("3 for exit");
+                Console.WriteLine("3 for Die rolls");
+                Console.WriteLine("4 for exit");
                 string choice = Console.ReadLine();
 
                 switch (choice)
@@ -24,7 +27,9 @@ namespace Topic_6
                         Bank();
                         break;
                     case "3":
-
+                        Die();
+                        break;
+                    case "4":
                         return;
                     default:
                         Console.WriteLine("Invalid menu choice.");
@@ -77,72 +82,118 @@ namespace Topic_6
             decimal depo;
             decimal bill;
             decimal withdrawal;
-            Console.WriteLine("WELCOME TO BoB, THE BEST BANK IN THE UNIVERSE");
-            Console.WriteLine();
-            Console.WriteLine("There are many options such as:");
-            Console.WriteLine("Deposit: Enter 1");
-            Console.WriteLine("Bill payment: Enter 2");
-            Console.WriteLine("Withdrawal: Enter 3");
-            Console.WriteLine("Account Balance Update: Enter 4");
-            Console.WriteLine("Exit: Enter 5");
-            Console.WriteLine($"You have {total:C} to use in this bank");
-            string choice = Console.ReadLine();
-            switch (choice)
+            bool inBank = true;
+            while (inBank)
             {
-                case "1":
-                    Console.WriteLine("To Deposit, enter the amount you'd like to deposit");
-                    while (!decimal.TryParse(Console.ReadLine(), out depo) || depo < 0)
-                        Console.WriteLine("Invalid Input, please try again");
-                    Console.WriteLine();
-                    Console.WriteLine($"You are going to deposit {depo:C}");
-                    Console.WriteLine("You are still going to be charged $0.75 for this transaction thought");
-                    total -= 0.75m;
-                    total += depo;
-                    Console.WriteLine($"Your total is now {total:C}.");
-                    
-                    break;
-                case "2":
-                    total -= 0.75m;
-                    Console.WriteLine("How much do you want to pay for your bill");
-                    while (!decimal.TryParse(Console.ReadLine(), out bill) || bill < 0)
-                        Console.WriteLine("Invalid Input, please try again");
-                    Console.WriteLine();
-                    total -= bill;
-                    Console.WriteLine($"You will pay {bill:C}. Your total is now {total:C}.");
-                    Console.WriteLine();
-                    break;
-                case "3":
-                    total -= 0.75m;
-                    Console.WriteLine("How much do you want to withdrawal?");
-                    while (!decimal.TryParse(Console.ReadLine(), out withdrawal) || withdrawal < 0)
-                        Console.WriteLine("Invalid Input, please try again");
-                    total -= withdrawal;
-                    Console.WriteLine($"You took out {withdrawal:C}. Your total is now {total:C}.");
-                    Console.WriteLine();
-                    break;
-                case "4":
-                    total -= 0.75m;
-                    Console.WriteLine($"Your account is now at {total:C}");
-                    Console.WriteLine();
-                    break;
-                case "5":
+                Console.Clear();
+                Console.WriteLine("WELCOME TO BoB, THE BEST BANK IN THE UNIVERSE");
+                Console.WriteLine();
+                Console.WriteLine("There are many options such as:");
+                Console.WriteLine("Deposit: Enter 1");
+                Console.WriteLine("Bill payment: Enter 2");
+                Console.WriteLine("Withdrawal: Enter 3");
+                Console.WriteLine("Account Balance Update: Enter 4");
+                Console.WriteLine("Exit: Enter 5");
+                Console.WriteLine($"You have {total:C} to use in this bank");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("To Deposit, enter the amount you'd like to deposit");
+                        while (!decimal.TryParse(Console.ReadLine(), out depo) || depo < 0)
+                            Console.WriteLine("Invalid Input, please try again");
+                        Console.WriteLine();
+                        Console.WriteLine($"You are going to deposit {depo:C}");
+                        Console.WriteLine("You are still going to be charged $0.75 for this transaction thought");
+                        total -= 0.75m;
+                        total += depo;
+                        Console.WriteLine($"Your total is now {total:C}.");
+
+                        break;
+                    case "2":
+                        Console.Clear();
+                        total -= 0.75m;
+                        Console.WriteLine("How much do you want to pay for your bill");
+                        while (!decimal.TryParse(Console.ReadLine(), out bill) || bill < 0)
+                            Console.WriteLine("Invalid Input, please try again");
+                        Console.WriteLine();
+                        total -= bill;
+                        Console.WriteLine($"You will pay {bill:C}. Your total is now {total:C}.");
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        total -= 0.75m;
+                        Console.WriteLine("How much do you want to withdrawal?");
+                        while (!decimal.TryParse(Console.ReadLine(), out withdrawal) || withdrawal < 0)
+                            Console.WriteLine("Invalid Input, please try again");
+                        total -= withdrawal;
+                        Console.WriteLine($"You took out {withdrawal:C}. Your total is now {total:C}.");
+                        Console.WriteLine();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        total -= 0.75m;
+                        Console.WriteLine($"Your account is now at {total:C}");
+                        Console.WriteLine();
+                        break;
+                    case "5":
+                        Console.Clear();
+                        inBank = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid menu choice.");
+                        Console.WriteLine("You will be charged $0.75");
+                        total -= 0.75m;
+                        break;
+
+
+                }
+                if (inBank)
+                {
+                    Console.WriteLine("Press enter to continue");
+                    Console.ReadLine();
+                }
+                if (total < 0)
+                {
+                    Console.WriteLine("You are lower then 0 in your account. You now have to leave");
                     return;
-                default:
-                    Console.WriteLine("Invalid menu choice.");
-                    Console.WriteLine("You will be charged $0.75");
-                    total -= 0.75m;
-                    break;
-
-
+                }
             }
-            if (total < 0)
+        }
+        public static void Die()
+        {
+            Random random = new Random();
+
+            int die1;
+            int die2;
+            int rollCount = 0;
+
+            do
             {
-                Console.WriteLine("You are lower then 0 in your account. You now have to leave");
-                return;
-            }
+                Console.Clear();
+
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                rollCount++;
+
+                Console.WriteLine($"Roll #{rollCount}");
+                Console.WriteLine($"Die 1: {die1}");
+                Console.WriteLine($"Die 2: {die2}");
+
+                if (die1 != die2)
+                {
+                    Console.WriteLine("Press enter to roll again");
+                    Console.ReadLine();
+                }
+            } while (die1 != die2);
+            Console.WriteLine($"You got doubles! It took {rollCount} rolls");
+
+                
 
         }
-    
     
     
     
